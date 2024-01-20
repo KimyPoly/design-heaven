f25a83bca2afa25af383e389e951cb3d8b070a7d
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-01-20 17:56:32 +0100

    F05 : Ajout fakeData

---
+++ src/fakeData/fakeFurnitures.jsx
@@ -0,0 +1,34 @@
+export const fakeFurnitures = [
+  {
+    id: 1,
+    img: "https://th.bing.com/th/id/OIG._DgpsAC1W3HFWX.oPhZk?pid=ImgGn",
+    title: "Pinky Pinky ",
+    description: "Immerse yourself in the luxury of comfort and aesthetics with our designer 3-seater sofa. Its pastel pink hue evokes the contemporary trend, adding a delicate touch to your space. Discover the perfect combination of style and functionality",
+    price: 569.99,
+    quantity: 0,
+  },
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/OIG.cy1LwVZoO6PQwPP15Itf?pid=ImgGn",
+    title: "Colory Pouf",
+    description: "Experience the extravagance of comfort with our giant multi-colored beanbag. An explosion of vibrant hues to brighten up your space with a touch of modernity. Immerse yourself in stylish well-being",
+    price: 284.40,
+    quantity: 0,
+  },
+  {
+    id: 3,
+    img: "https://th.bing.com/th/id/OIG.kUx84gkoCt2wr2RHEl7n?pid=ImgGn",
+    title: "Yellow Confort",
+    description: "Brighten up your space with our yellow armchair, synonymous with style and comfort. The vibrant shade brings a touch of vitality to your interior. Make every moment sunny",
+    price: 50.20,
+    quantity: 0,
+  },
+  {
+    id: 4,
+    img: "https://th.bing.com/th/id/OIG.ryuIT8RKU9Jk4nOWJQQh?w=1024&h=1024&rs=1&pid=ImgDetMain",
+    title: "Yinky Blue",
+    description: "Explore color harmony with our unique armchair in blue, pink and a touch of yellow. A bold fusion of trendy hues for a seat that transcends conventional design.",
+    price: 54.99,
+    quantity: 0,
+  },
+]
