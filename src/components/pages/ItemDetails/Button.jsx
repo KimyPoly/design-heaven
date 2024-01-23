66db1413b2b58d2dd09a4fc8b1cda29a873189ca
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-01-23 17:30:10 +0100

    💄 F06 : ITEMDETAILS > style button

--- src/components/pages/ItemDetails/Button.jsx
+++ src/components/pages/ItemDetails/Button.jsx
@@ -9,9 +9,18 @@ export default function Button() {
   )
 }
 const ButtonStyled = styled.button`
+  background: #FF9843;
+  margin: 2em;
+  padding: 1.5em;
+  border: none;
+  border-radius: 16px;
+
   a {
     text-decoration: none;
-    
+    font-family: 'Satisfy', cursive;
+    color: white;
+    font-size: 1.5em;
+
   }
 
 `;
