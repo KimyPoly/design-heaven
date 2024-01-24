25b84c500c0b43bd4c948a3018ef16fa1b072b77
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-01-24 14:39:24 +0100

    💄 F06 : ITEMDETAILS > Modif style

--- src/components/pages/ItemDetails/ItemRight.jsx
+++ src/components/pages/ItemDetails/ItemRight.jsx
@@ -16,17 +16,27 @@ export default function ItemRight({title, price, description}) {
 const ItemRightStyled = styled.div`
   height: 100vh;
   text-align: center;
+  font-family: 'Satisfy', cursive;
+
   h1 {
-    font-size: 4em;
+    font-size: 4.5em;
     margin-top: 2em;
-    color:#e66465;
+    color:white;
+    box-shadow: 60px 16px #e66465;
   }
 
   p {
     margin: 2em;
-    color:#FF9843;
-    border: 2px solid white;
+    color: white;
+    border: 4px solid #FF9843;
     padding: 1em;
+    font-size: 1em;
   }
 
-`;
+  h3 {
+    color: white;
+    font-size: 3em;
+    margin-bottom: 0;
+  }
+
+`
