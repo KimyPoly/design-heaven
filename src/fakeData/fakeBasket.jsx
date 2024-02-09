38c62a5042ee619ede66842976781f55f86d5ed4
Kimberley Polypheme <kim.polypheme@gmail.com>
2024-02-09 19:03:20 +0100

    ✨ F12: Creation fakeBasket.jsx

---
+++ src/fakeData/fakeBasket.jsx
@@ -0,0 +1,18 @@
+export const EMPTY = []
+
+export const SMALL = [
+  {
+    id: "1",
+    title: "Colory Pouf",
+    img: "https://th.bing.com/th/id/OIG.cy1LwVZoO6PQwPP15Itf?pid=ImgGn",
+    price: 284.42,
+    quantity: 1,
+  },
+  {
+    id: "2",
+    title: "Burger Meal",
+    img: "https://th.bing.com/th/id/OIG.kUx84gkoCt2wr2RHEl7n?pid=ImgGn",
+    price: 50.20,
+    quantity: 3,
+  },
+]
